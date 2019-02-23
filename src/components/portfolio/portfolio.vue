0d74ce65586748b0dfebdcf1952febc2b69a63ea
Mike Ludemann <mike-ludemann@hotmail.de>
2019-02-23 19:15:37 +0100

    portfolio are now available

---
+++ src/components/portfolio/portfolio.vue
@@ -0,0 +1,75 @@
+<template lang="html">
+	<div class="content" :style="content">
+		<img v-bind:src="url" v-bind:alt="alttext" :style="image"/>
+		<div class="info" :style="info">
+			<h1 :style="headlineStyle">{{this.headline}}</h1>
+			<p class="block" :style="block">{{this.text}}</p>
+			<p class="block" :style="block">&copy; {{this.name}} - {{this.year}}</p>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+	export default {
+		name: 'portfolio',
+		props: {
+      url: String,
+      alttext: String,
+      headline: String,
+      text: String,
+      name: String,
+      year: String
+    },
+		mounted() {
+
+		},
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		},
+		computed: {
+			content: function() {
+        return {
+          backgroundColor: 'white',
+          border: '0.5px solid #000',
+        }
+			},
+
+      info: function() {
+        return {
+          padding: '15px 10px',
+        }
+      },
+
+      image: function() {
+        return {
+          width: '100%',
+          /* opacity: 1; */
+        }
+      },
+
+      headlineStyle: function() {
+        return {
+          fontSize: '24px',
+          paddingBottom: '10px',
+        }
+      },
+
+      block: function() {
+        return {
+          padding: '10px 0px',
+        }
+      }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+	.portfolio {
+
+	}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,24 +1,27 @@
 <template>
 	<div class="about">
 		<h1>This is an about page</h1>
-			<button type="button" class="btn" @click="showModal">
-				Open Modal!
-			</button>
-			<modal v-if="isModalVisible" @close="closeModal">
-				<h2 slot="header">My header</h2>
-				<div slot="body">this is the body</div>
-				<div slot="footer">Copyright</div>
-			</modal>
+		<button type="button" class="btn" @click="showModal">
+			Open Modal!
+		</button>
+		<modal v-if="isModalVisible" @close="closeModal">
+			<h2 slot="header">My header</h2>
+			<div slot="body">this is the body</div>
+			<div slot="footer">Copyright</div>
+		</modal>
+		<portfolio year="2018" name="Testament" headline="Snow Mountain" url="https://images.unsplash.com/photo-1488818138649-d2734488e6d0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80" alttext="Mountains" text="Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no."></portfolio>
 	</div>
 </template>
 
 <script>
 	import modal from '@/components/modal/modal.vue';
+	import portfolio from '@/components/portfolio/portfolio.vue';
 
 	export default {
 		name: 'about',
 		components: {
 			modal,
+			portfolio,
 		},
 		data () {
 			return {
