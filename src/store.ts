61c468fc919f4289231aea0481e6917b4242dd60
Mike Ludemann <mike-ludemann@hotmail.de>
2019-03-27 14:36:52 +0100

    Simple functions and states in Vuex

--- src/store.ts
+++ src/store.ts
@@ -4,13 +4,57 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  },
+	state: {
+		count: 0,
+		lists: [
+			{ id: 1, task: "Home Office", done: true },
+			{ id: 2, task: "Travel", done: true },
+			{ id: 3, task: "Clean the apartment", done: false }
+		]
+	},
+	getters: {
+		finishList: state => {
+			return state.lists.filter(list => list.done)
+		},
+		openList: state => {
+			return state.lists.filter(list => !list.done)
+		},
+		getListElementById: (state) => (id: number) => {
+			return state.lists.find(list => list.id === id)
+		}
+	},
+	mutations: {
+		increment (state) {
+			if(state.count == 99){
+				state.count = 99
+			}
+			state.count++
+		},
+		incrementBy (state, n) {
+			if(state.count == 99 || (state.count += n) >= 99){
+				state.count = 99
+			}
+			state.count += n
+		},
+		decrement (state) {
+			if(state.count == 0){
+				state.count = 0
+			}
+			state.count--
+		},
+		decrementBy (state, n) {
+			if(state.count == 0 || (state.count -= n) <= 0){
+				state.count = 0
+			}
+			state.count--
+		}
+	},
+	actions: {
+		increment (context) {
+			context.commit('increment')
+		},
+		decrement (context) {
+			context.commit('decrement')
+		}
+	},
 });
